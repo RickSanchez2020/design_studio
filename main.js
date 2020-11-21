e8540ddce4531c5dfce522bedfa4d4d530968e31
RickSanchez2020 <65333057+RickSanchez2020@users.noreply.github.com>
2020-11-21 12:39:20 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+let modal = document.getElementById('modal');
+menuOpen = () => {
+  modal.style.height = '100vh';
+  modal.style.padding = '20px';
+};
+menuClose = () => {
+  modal.style.height = '0';
+  modal.style.padding = '0';
+};
